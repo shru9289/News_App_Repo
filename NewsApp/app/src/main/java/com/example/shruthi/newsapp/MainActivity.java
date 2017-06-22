f4c42eeb186ab108b4c1a843013accad9b760bdb
shru9289 <sjain8@calstatela.edu>
2017-06-21 21:47:50 -0700

    NewsApp Feature with Insert apikey

---
+++ NewsApp/app/src/main/java/com/example/shruthi/newsapp/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.shruthi.newsapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progress;
+    private EditText search;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progress =(ProgressBar) findViewById(R.id.progressBar);
+        search =(EditText)findViewById(R.id.searchQuery);
+        textView=(TextView)findViewById(R.id.displayJSON);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemNumber = item.getItemId();
+
+        if(itemNumber==R.id.search){
+            String s=search.getText().toString();
+            NetworkTask task = new NetworkTask(s);
+            task.execute();
+        }
+        return true;
+    }
+    class NetworkTask extends AsyncTask<URL,Void,String>{
+        String query;
+
+        NetworkTask(String s){
+            query =s;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progress.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            String result=null;
+            URL url = NetworkUtils.makeURL(query,"stars");
+
+            try{
+                result  = NetworkUtils.getResponseFromHttpUrl(url);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progress.setVisibility(View.GONE);
+            if(s == null){
+                textView.setText("Sorry, no text was received ");
+            }
+            else
+            {
+                textView.setText(s);
+            }
+
+        }
+    }
+
+}
---
+++ NewsApp/app/src/main/java/com/example/shruthi/newsapp/NetworkUtils.java
@@ -0,0 +1,56 @@
+package com.example.shruthi.newsapp;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+/**
+ * Created by Shruthi on 6/12/2017.
+ */
+
+public class NetworkUtils {
+    public static final String TAG ="NetworkUtils";
+
+    public static final String BASE_URL="https://newsapi.org/v1/articles?";
+    public static final String PARAM_QUERY="source";
+    public static final String PARAM_SORT="sortBy";
+    public static final String apiKey="apiKey";
+    // ToDO Insert your api key in the APPLY_API_KEY
+    public static final String APPLY_API_KEY="1303c407d5f2459999f6a170cd783347";
+
+
+    public static URL makeURL(String searchQuery, String sortBy){
+        Uri uri= Uri.parse(BASE_URL).buildUpon().appendQueryParameter(PARAM_QUERY,"the-next-web")
+                .appendQueryParameter(PARAM_SORT,"latest")
+                .appendQueryParameter(apiKey,APPLY_API_KEY).build();
+
+        URL url =null;
+        try{
+            String urlString= uri.toString();
+            Log.d(TAG,"URL" +urlString);
+            url = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+    public static String getResponseFromHttpUrl(URL url) throws IOException{
+        HttpURLConnection urlConnection =(HttpURLConnection) url.openConnection();
+        try{
+            InputStream in =urlConnection.getInputStream();
+            Scanner input = new Scanner(in);
+
+            input.useDelimiter("\\A");
+            return(input.hasNext()) ? input.next():null;
+        }finally{
+            urlConnection.disconnect();
+        }
+    }
+}
